1f19f6ebb6f7c95f7266541481ea851d2db6f5c7
LegoBrainBiker <sameer.brink26@scdsstudent.org>
2024-10-05 16:31:01 -0700

    made camera work verticaly

--- camera_3d.gd
+++ camera_3d.gd
@@ -14,3 +14,4 @@ func _input(event):
 	if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
 		if event is InputEventMouseMotion:
 			rotate_object_local(Vector3.UP, event.relative.x*0.001)
+			rotate_object_local(Vector3.RIGHT, event.relative.y*0.001)
