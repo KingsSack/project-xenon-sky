116ad281abbcafbe4a8e91a4eb4cd458844d1f9b
KingsSack <sanjaybrink@gmail.com>
2024-10-05 16:41:17 -0700

    Clean up scripts

--- scripts/load_stars.gd
+++ scripts/load_stars.gd
@@ -1,10 +1,17 @@
 extends Node3D
 
+var data
+
+# Called when the node enters the scene tree for the first time.
 func _ready():
-	var url = "https://gea.esac.esa.int/tap-server/tap/tables/sync?query=select+top+100+designation,ra,dec,parallax+from+gaiadr3.gaia_source+order+by+source_id&format=json"
+	query_database("https://exoplanetarchive.ipac.caltech.edu/TAP/sync?query=select+top+100+pl_name,ra,dec,parallax+from+ps&format=json")
+	var exoplanet_data = data
+	
+	query_database("https://gea.esac.esa.int/tap-server/tap/tables/sync?query=select+top+100+designation,ra,dec,parallax+from+gaiadr3.gaia_source+order+by+source_id&format=json")
+	var star_data = data
+
+func query_database(url):
 	var headers = []  # No headers needed
-	
-	
 	
 	var response = HTTPRequest.new()
 	add_child(response)
@@ -19,22 +26,8 @@ func _on_query_completed(_result, response_code, _headers, body):
 		var json = JSON.new()
 		var json_response = json.parse(body.get_string_from_utf8())
 		if json_response == OK:
-			print(json_response)
+			data = json_response
 		else:
 			print("Error parsing JSON: ", json_response)
 	else:
 		print("Query failed with status: ", response_code)
-
-var x = []
-
-func make_star(x1):
-	x.append(x1)
-var y
-func instace_scene():
-	var scene = preload("res://map.tscn")
-	var scene_instance = scene.instance()
-	scene_instance.set_name("scene")
-	for pos in x:
-		y = Node3D
-		y.transform(pos)
-		y.new().add_child(scene_instance)
