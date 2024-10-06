8e00d82249c3c8cac269e0e1af0ac34f1ec9c119
kyin27 <155483551+kyin27@users.noreply.github.com>
2024-10-06 11:19:55 -0700

    ei ei ei

--- control.gd
+++ control.gd
@@ -3,6 +3,7 @@ extends Control
 @export var tween_duration: float
 
 @onready var play: Button = $Button
+@onready var galxy: TextureRect = $TextureRect
 
 func _process(delta: float) -> void:
 	btn_hovered(play)
@@ -15,8 +16,10 @@ func btn_hovered(button: Button):
 	button.pivot_offset = button.size/2
 	if button.is_hovered():
 		start_tween(button, "scale", Vector2.ONE * tween_intensity, tween_duration)
+		galxy.visible = true
 	else:
 		start_tween(button, "scale", Vector2.ONE, tween_duration)
+		galxy.visible = false
 
 
 # Called when the node enters the scene tree for the first time.
