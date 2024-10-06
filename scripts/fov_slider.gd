ab481f793cd98292dcbdf648ce1b2904fd4e0614
LegoBrainBiker <sameer.brink26@scdsstudent.org>
2024-10-06 14:50:26 -0700

    fixed

--- scripts/fov_slider.gd
+++ scripts/fov_slider.gd
@@ -3,7 +3,7 @@ extends VSlider
 signal slider_changed(new_value, not_draging)
 
 func _process(_delta: float) -> void:
-	slider_changed.emit((120 - value) / 1.6)
+	slider_changed.emit((120 - value) / 1.6, is_not_draging)
 
 var is_not_draging
 
