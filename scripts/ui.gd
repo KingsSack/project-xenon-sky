0ed67bedc996c938b6305c1cc8163a6dddf9a8a5
kyin27 <155483551+kyin27@users.noreply.github.com>
2024-10-06 09:59:29 -0700

    me

--- scripts/ui.gd
+++ scripts/ui.gd
@@ -4,6 +4,7 @@ var matches = []
 
 func _on_line_edit_text_changed(new_text: String) -> void:
 	var items = $ScrollContainer/VBoxContainer.get_children()
+	print(items)
 	if new_text == "":
 		for i in items:
 			i.visible = true
