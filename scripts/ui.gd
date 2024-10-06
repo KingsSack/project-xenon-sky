2b8d9535f7a21d7822956dea7f2d068f20c2ce22
kyin27 <155483551+kyin27@users.noreply.github.com>
2024-10-06 12:56:34 -0700

    toggle

--- scripts/ui.gd
+++ scripts/ui.gd
@@ -1,7 +1,8 @@
 extends Control
 
 var matches = []
-
+@onready var menu = $ScrollContainer
+@onready var search = $LineEdit
 func _on_line_edit_text_changed(new_text: String) -> void:
 	var items = $ScrollContainer/VBoxContainer.get_children()
 	if new_text == "":
@@ -17,3 +18,14 @@ func _on_line_edit_text_changed(new_text: String) -> void:
 			i.visible = true
 		else:
 			i.visible = false
+
+
+func _on_button_pressed() -> void:
+	menu.visible = false
+	search.visible = false
+	
+
+
+func _on_button_2_pressed() -> void:
+	menu.visible = true
+	search.visible = true
