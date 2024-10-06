8bbad05f940858dd7ebbdfd1927cc83abd82100f
LegoBrainBiker <sameer.brink26@scdsstudent.org>
2024-10-06 14:48:33 -0700

    zoom better

--- scripts/camera_3d.gd
+++ scripts/camera_3d.gd
@@ -2,12 +2,18 @@ extends Camera3D
 
 func _ready():
 	get_node("/root/Node3D/CanvasLayer/Control/VSlider").slider_changed.connect(_on_slider_changed)
+	
 
 func _input(event):
-	if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
+	if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT) and notslidering:
 		if event is InputEventMouseMotion:
 			rotate_object_local(Vector3.UP, event.relative.x*0.002)
 			rotate_object_local(Vector3.RIGHT, event.relative.y*0.002)
 
-func _on_slider_changed(new_value):
+var oldfov
+
+func _on_slider_changed(new_value, not_draging):
+	notslidering = not_draging
 	set_fov(new_value+1)
+	oldfov = new_value
+var notslidering
--- scripts/fov_slider.gd
+++ scripts/fov_slider.gd
@@ -1,6 +1,14 @@
 extends VSlider
 
-signal slider_changed(new_value)
+signal slider_changed(new_value, not_draging)
 
 func _process(_delta: float) -> void:
 	slider_changed.emit((120 - value) / 1.6)
+
+var is_not_draging
+
+func _on_drag_started():
+	is_not_draging = false
+
+func _on_drag_ended():
+	is_not_draging = true
