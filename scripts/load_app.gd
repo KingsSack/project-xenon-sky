921a0ce0ecd2a3520720b57b0573f6459ebcd381
KingsSack <sanjaybrink@gmail.com>
2024-10-05 20:41:00 -0700

    Add new exoplanet loading functionality and refactor scene structure

---
+++ scripts/load_app.gd
@@ -0,0 +1,73 @@
+extends Node3D
+
+var button_scene := preload("res://load_planet_button.tscn")
+
+var exoplanets = {}
+var stars = {}
+
+signal exoplanet_data_loaded(data)
+signal star_data_loaded(data)
+
+func _ready():
+	query_database("https://exoplanetarchive.ipac.caltech.edu/TAP/sync?query=SELECT+top+10+pl_name,ra,dec,sy_plx+FROM+ps+ORDER+BY+pl_name&format=json", "exoplanets")
+	query_database("https://gea.esac.esa.int/tap-server/tap/sync?request=doQuery&lang=ADQL&query=SELECT+TOP+25+designation,ra,dec,parallax+FROM+gaiadr3.gaia_source+WHERE+parallax+IS+NOT+NULL+ORDER+BY+source_id&format=json", "stars")
+
+func query_database(url, key):
+	var headers = []
+	var response = HTTPRequest.new()
+	add_child(response)
+	response.connect("request_completed", Callable(self, "_on_query_completed").bind(key))
+	response.request(url, headers)
+
+func _on_query_completed(_result, response_code, _headers, body, key):
+	# print("Raw body: ", body.get_string_from_utf8())
+	if response_code == 200:
+		var json = JSON.new()
+		var decoded_body = body.get_string_from_utf8()
+		var error = json.parse(decoded_body)
+		if error == OK:
+			# print("Successfully parsed JSON: ", json.data)
+			if key == "exoplanets":
+				exoplanet_data_loaded.emit(json.data)
+			elif key == "stars":
+				star_data_loaded.emit(json.data)
+		else:
+			print("Error parsing JSON: ", error)
+	else:
+		print("Query failed with status: ", response_code)
+
+func _on_exoplanet_data_loaded(data):
+	for exoplanet in data:
+		create_exoplanet_button(exoplanet["pl_name"])
+		exoplanets[exoplanet["pl_name"]] = [exoplanet["ra"], exoplanet["dec"], exoplanet["sy_plx"]]
+		# print("Exoplanet: ", exoplanet["pl_name"], " at (", exoplanet["ra"], ", ", exoplanet["dec"], ") with parallax: ", exoplanet["sy_plx"])
+
+func _on_star_data_loaded(data):
+	for star in data["data"]:
+		stars[star[0]] = [star[1], star[2], star[3]]
+		# print("Star: ", star[0], " at (", star[1], ", ", star[2], ") with parallax: ", star[3])
+
+func create_exoplanet_button(planet_name):
+	var button_instance = button_scene.instantiate()
+	button_instance.text = planet_name
+	$CanvasLayer/Control/ScrollContainer/VBoxContainer.add_child(button_instance)
+
+var x = []
+
+func make_star(x1):
+	x.append(x1)
+var y
+func instace_scene():
+	var scene = preload("res://map.tscn")
+	var scene_instance = scene.instance()
+	scene_instance.set_name("scene")
+	for pos in x:
+		y = Node3D
+		y.transform(pos)
+		y.new().add_child(scene_instance)
+
+func convert_ra_and_dec(ra, dec, parallax):
+	var x = cos(dec) * cos(ra)/tan(parallax)
+	var y = cos(dec) * sin(ra)/tan(parallax)
+	var z = sin(dec)/tan(parallax)
+	return Vector3(x,y,z)
---
+++ scripts/new_exoplanet.gd
@@ -0,0 +1,5 @@
+extends Node
+
+
+func _on_load_exoplanet_pressed():
+	pass # Replace with function body.
