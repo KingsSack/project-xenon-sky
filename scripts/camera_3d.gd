66f6568e7c6e4da6b5dada19a326e4e3da3dc90e
LegoBrainBiker <sameer.brink26@scdsstudent.org>
2024-10-06 13:16:17 -0700

    made camera scrolling faster

--- scripts/camera_3d.gd
+++ scripts/camera_3d.gd
@@ -3,5 +3,5 @@ extends Camera3D
 func _input(event):
 	if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
 		if event is InputEventMouseMotion:
-			rotate_object_local(Vector3.UP, event.relative.x*0.001)
-			rotate_object_local(Vector3.RIGHT, event.relative.y*0.001)
+			rotate_object_local(Vector3.UP, event.relative.x*0.002)
+			rotate_object_local(Vector3.RIGHT, event.relative.y*0.002)
