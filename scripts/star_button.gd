4d89ee4c8c45aa867c7e6783216e2bb0b13dcb7e
KingsSack <sanjaybrink@gmail.com>
2024-10-06 14:07:22 -0700

    Remove debug print statement

--- scripts/star_button.gd
+++ scripts/star_button.gd
@@ -6,5 +6,4 @@ func _process(_delta):
 	position = get_viewport().get_camera_3d().unproject_position(star.global_transform.origin)
 
 func _on_pressed():
-	print("Star button pressed")
 	Global.add_star(star)
