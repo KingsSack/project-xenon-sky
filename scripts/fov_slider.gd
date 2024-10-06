8c6ef17fa20275e214a835d626e13e2bff3fd3b2
KingsSack <sanjaybrink@gmail.com>
2024-10-06 15:26:05 -0700

    Actually fixed!

--- scripts/fov_slider.gd
+++ scripts/fov_slider.gd
@@ -3,12 +3,14 @@ extends VSlider
 signal slider_changed(new_value, not_draging)
 
 func _process(_delta: float) -> void:
-	slider_changed.emit((120 - value) / 1.6, is_not_draging)
+	slider_changed.emit((120 - value) / 1.6, is_not_dragging)
 
-var is_not_draging
+var is_not_dragging = true
 
 func _on_drag_started():
-	is_not_draging = false
+	print("test 1")
+	is_not_dragging = false
 
-func _on_drag_ended():
-	is_not_draging = true
+func _on_drag_ended(_value_changed):
+	print("test 2")
+	is_not_dragging = true
