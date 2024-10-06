d6f02ee7a48b7ba3c2877399dcc5e4bc8904177f
KingsSack <sanjaybrink@gmail.com>
2024-10-06 16:22:19 -0700

    Update loading progress

--- loading_progress.gd
+++ loading_progress.gd
@@ -1,8 +1,10 @@
 extends Label
 
+@onready var node := get_node("/root/Node3D")
+
 func _process(_delta):
-	if get_node("/root/Node3D").exoplanet_data_recieved or get_node("/root/Node3D").star_data_recieved:
-		if get_node("/root/Node3D").exoplanet_data_recieved and get_node("/root/Node3D").star_data_recieved:
+	if node.exoplanet_data_recieved or node.star_data_recieved:
+		if node.exoplanet_data_recieved and node.star_data_recieved:
 			text = "2/2"
 		else:
 			text = "1/2"
